c584873682fd421c3182c847bb359c3a61093df5
Emi Hayakawa <52098983+hayamin1111@users.noreply.github.com>
2023-06-23 22:16:18 +0900

    add scroll animation

--- src/js/scrollAnim.js
+++ src/js/scrollAnim.js
@@ -1,4 +1,4 @@
-import {gsap} from "gsap";
+import {gsap, Power4} from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
@@ -6,18 +6,41 @@ gsap.registerPlugin(ScrollTrigger);
  * @descroption スクロールアニメーション
  */
 export default () => {
+  gsap.set('.js-handdrawnImage',  {
+    opacity: 0,
+    y: "20%",
+    filter: "blur(10px)",
+  });
   gsap.to('.js-handdrawnImage', {
+    opacity: 1,
+    y: 0,
     filter: "blur(0)",
-    ease: 'linear',
-    duration: 5,
+    ease: Power4.out,
+    duration: .7,
     scrollTrigger: {
       trigger: "#js-handdrawn",
-      start: "top center",
+      start: "top 80%",
       markers: true,
     },
     stagger: {
       from: "start", 
-      amount: 0.4,
+      amount: 0.7,
+    },
+  })
+  gsap.to('.js-pickupSubtitle', {
+    opacity: 1,
+    filter: "blur(0)",
+    rotateY: "0deg",
+    ease: Power4.out,
+    duration: .5,
+    scrollTrigger: {
+      trigger: "#js-pickup",
+      start: "top 80%",
+      markers: true,
+    },
+    stagger: {
+      from: "start", 
+      amount: 0.3,
     },
   })
 }
